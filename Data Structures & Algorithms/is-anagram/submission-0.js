50c683d7d3be556f29a6f563ed5d079d2512cabd
Dibyajyoti <92978569+dibyajyoti79@users.noreply.github.com>
2026-07-18 22:21:05 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        const freq = new Map();
+
+        for (const ch of s) {
+            freq.set(ch, (freq.get(ch) || 0) + 1);
+        }
+
+        for (const ch of t) {
+            if (!freq.has(ch)) {
+                return false;
+            }
+
+            freq.set(ch, freq.get(ch) - 1);
+
+            if (freq.get(ch) < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
